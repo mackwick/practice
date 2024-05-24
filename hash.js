354cc199051264a32bd09ce087d8bb49a090385a
mackwick <mackenziewicker@gmail.com>
2024-05-24 14:41:59 -0400

    reviewed some hash stuff

--- hash.js
+++ hash.js
@@ -3,58 +3,77 @@
 //Collision - two values given the same key (solution is using something like an array or linked list to track all)
 //At its core, it is an array-based data structure with some added functionality
 
-const hash = (key, size) => {
-  let hashedKey = 0;
+// const hash = (key, size) => {
+//   let hashedKey = 0;
 
-  for (let i = 0; i < key.length; i++) {
-    hashedKey = key.charCodeAt(i);
-  }
+//   for (let i = 0; i < key.length; i++) {
+//     hashedKey = key.charCodeAt(i);
+//   }
 
-  return hashedKey % size;
-};
+//   return hashedKey % size;
+// };
 
+// class HashTable {
+//   constructor() {
+//     this.size = 20;
+//     this.buckets = Array(this.size);
+
+//     for (let i = 0; i < this.buckets.length; i++) {
+//       this.buckets[i] = new Map();
+//     }
+//   }
+
+//   insert(key, value) {
+//     let idx = hash(key, this.size);
+//     this.buckets[idx].set(key, value);
+//   }
+
+//   remove(key) {
+//     let idx = hash(key, this.size);
+//     let deleted = this.buckets[idx].get(key);
+//     this.buckets[idx].delete(key);
+//     return deleted;
+//   }
+
+//   search(key) {
+//     let idx = hash(key, this.size);
+//     return this.buckets[idx].get(key);
+//   }
+// }
+
+// const hashTable = new HashTable();
+
+// hashTable.insert("serena", "moon");
+// hashTable.insert("amy", "mercury");
+// hashTable.insert("rei", "mars");
+// hashTable.insert("lita", "jupiter");
+// hashTable.insert("mina", "venus");
+// hashTable.insert("darien", "tuxedo max");
+
+// console.log(hashTable.search("rei"));
+// console.log(hashTable.search("lita"));
+// console.log(hashTable.search("serena"));
+
+// hashTable.remove("darien");
+// hashTable.remove("mina");
+
+// console.log(hashTable);
+
+//HARDWARE INVENTORY EXAMPLE
 class HashTable {
-  constructor() {
-    this.size = 20;
-    this.buckets = Array(this.size);
-
-    for (let i = 0; i < this.buckets.length; i++) {
-      this.buckets[i] = new Map();
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
-  }
-
-  insert(key, value) {
-    let idx = hash(key, this.size);
-    this.buckets[idx].set(key, value);
-  }
-
-  remove(key) {
-    let idx = hash(key, this.size);
-    let deleted = this.buckets[idx].get(key);
-    this.buckets[idx].delete(key);
-    return deleted;
-  }
-
-  search(key) {
-    let idx = hash(key, this.size);
-    return this.buckets[idx].get(key);
+    return hash;
   }
 }
 
-const hashTable = new HashTable();
+let myHashTable = new HashTable();
+console.log(myHashTable);
 
-hashTable.insert("serena", "moon");
-hashTable.insert("amy", "mercury");
-hashTable.insert("rei", "mars");
-hashTable.insert("lita", "jupiter");
-hashTable.insert("mina", "venus");
-hashTable.insert("darien", "tuxedo max");
-
-console.log(hashTable.search("rei"));
-console.log(hashTable.search("lita"));
-console.log(hashTable.search("serena"));
-
-hashTable.remove("darien");
-hashTable.remove("mina");
-
-console.log(hashTable);
+//https://www.youtube.com/watch?v=CABNL8A_eVc&list=PLdPTfo6Ung1D9uRZXHSCfFdadAKMHaCNQ&index=4
