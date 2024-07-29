8b833a7c6872422494f1b0a4b7cfcf9d3da8c1ab
mackwick <mackenziewicker@gmail.com>
2024-07-29 12:58:04 -0400

    leetcode practice

--- interviewQs.js
+++ interviewQs.js
@@ -304,3 +304,42 @@ var maxProfit = function (prices) {
 };
 
 //O(n) ---> just going through the whole thing once
+
+/************************************** */
+// /**        Merge Two Sorted Lists            */ -----> did July 29, do again soooooon
+/************************************** */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  let dummy = new ListNode();
+  tail = dummy;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      tail.next = list1;
+      list1 = list1.next;
+    } else {
+      tail.next = list2;
+      list2 = list2.next;
+    }
+    tail = tail.next;
+  }
+
+  if (list1) {
+    tail.next = list1;
+  } else if (list2) {
+    tail.next = list2;
+  }
+
+  return dummy.next;
+};
